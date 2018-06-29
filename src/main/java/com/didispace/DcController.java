29bd21651dab4e4c42cb7a4b2576cfee61be24d2
zhoukun6688 <18333658323@163.com>
2018-06-29 16:05:08 +0800

    第三次提交

--- src/main/java/com/didispace/DcController.java
+++ src/main/java/com/didispace/DcController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,10 +24,22 @@ public class DcController {
     }
 
     public List<String> TestList(){
-
+        List<String> strList=testSTring(new ArrayList<>());
+        if (strList!=null && strList.size()>0){
+            for (String s:strList){
+                if (s!=null && s.length()>0){
+                    System.out.println(s);
+                }
+            }
+        }
         return null;
     }
 
-
+     private ArrayList<String> testSTring(ArrayList<String> list){
+         for (int i = 0; i <10 ; i++) {
+             list.add(i+"");
+         }
+        return list;
+     }
 
 }
