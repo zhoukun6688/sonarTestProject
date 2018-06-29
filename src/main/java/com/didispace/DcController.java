f934b889ccb9a2deee26c32487e1c5af47aa9590
zhoukun6688 <18333658323@163.com>
2018-06-29 15:50:39 +0800

    第二次提交

--- src/main/java/com/didispace/DcController.java
+++ src/main/java/com/didispace/DcController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 public class DcController {
@@ -19,5 +21,12 @@ public class DcController {
         System.out.println(services);
         return services;
     }
+
+    public List<String> TestList(){
+
+        return null;
+    }
+
+
 
 }
